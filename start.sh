d4c9862efdaec9ca0644cb45d30edee103fc38ac
thiagosoaresfelipe622-prog <thiagosoaresfelipe622@gmail.com>
2025-08-24 09:08:24 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Inicia o servidor de Minecraft em background
-java -Xmx512M -Xms512M -jar server.jar nogui &
-
 # Autentica no Ngrok
 ngrok config add-authtoken 31jQBc5dpSbMfkvmyopvOSM6Z3X_7dGABE4CHqHUY7F7D9bsJ
 
-# Cria túnel TCP na porta 25565
-ngrok tcp 25565
+# Inicia o túnel TCP em background
+ngrok tcp 25565 > /dev/null &
+
+# Inicia o servidor de Minecraft em primeiro plano
+java -Xmx1024M -Xms1024M -jar server.jar nogui
